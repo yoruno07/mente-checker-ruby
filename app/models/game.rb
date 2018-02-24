f8e37342ceeeb1864fce44a861d2c5a7921897af
yoruno07 <ryo.yoru.io@gmail.com>
2018-02-24 17:54:51 +0900

    Gameモデルのバリデーションを追加

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   has_many :keywords, dependent: :destroy
   has_many :checkers, dependent: :destroy
+  validates :name, presence: true
+  validates :account, presence: true
 
   def getKeywordsToSearch
     keywords.pluck('word').join(' OR ')
